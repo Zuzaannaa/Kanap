67d23d58f522e8540944352e77feb26a1e77f7a7
ZuzaL <zuzalubb@gmail.com>
2022-06-06 18:34:30 -0600

    fixes

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,97 +1,95 @@
-const cartItemsArray = [];
-for (i = 0; i < localStorage.length; i++) {
-  const key = localStorage.key(i);
-  const sofa = localStorage.getItem(key);
-  cartItemsArray.push(JSON.parse(sofa));
-}
-
-let itemQuantity;
-let totalQuantity = document.getElementById("totalQuantity");
-let sumQuantity = 0;
-let totalPrice = document.getElementById("totalPrice");
-let sumPrice = 0;
-let deleteItems;
-let numToDelete;
-let itemQuantityValue;
-
+const cart = createCart();
 showItemsinCart();
+order();
 
-//Show items in cart
-function showItemsinCart() {
-  for (let i = 0; i < cartItemsArray.length; i++) {
-    let cartItems = document.getElementById("cart__items");
-    if (cartItemsArray[i] != undefined) {
-      cartItems.innerHTML += `
-                                <article class="cart__item" data-id="${cartItemsArray[i]._id}" data-color="${cartItemsArray[i].selectedColor}">
-                                                <div class="cart__item__img">
-                                                <img src=${cartItemsArray[i].imageUrl} alt=${cartItemsArray[i].altTxt}>
-                                                </div>
-                                                <div class="cart__item__content">
-                                                <div class="cart__item__content__description">
-                                                    <h2>${cartItemsArray[i].name}</h2>
-                                                    <p>${cartItemsArray[i].selectedColor}</p>
-                                                    <p>${cartItemsArray[i].price}</p>
-                                                </div>
-                                                <div class="cart__item__content__settings">
-                                                    <div class="cart__item__content__settings__quantity">
-                                                    <p>Qté : </p>
-                                                    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${cartItemsArray[i].selectedQuantity}>
-                                                    </div>
-                                                    <div class="cart__item__content__settings__delete">
-                                                    <p class="deleteItem">Delete</p>
-                                                    </div>
-                                                </div>
-                                                </div>
-                                            </article>
-                                `;
-
-      deleteItems = document.querySelectorAll(".deleteItem");
-
-      for (const deleteItem of deleteItems) {
-        deleteItem.addEventListener("click", function (event) {
-          console.log("cartItemsArray[i]._id " + cartItemsArray[i]._id);
-          console.log("deleteItem[i] " + deleteItem[i]);
-          console.log(deleteItem);
-          this.style.backgroundColor = "red";
-        });
-      }
-
-      itemQuantityValue = document.querySelectorAll(
-        'input[name="itemQuantity"]'
-      );
-
-      let total =
-        Number(cartItemsArray[i].selectedQuantity) *
-        Number(cartItemsArray[i].price);
-      sumPrice += total;
-      totalPrice.innerHTML = sumPrice;
-
-      sumQuantity += Number(cartItemsArray[i].selectedQuantity);
-      totalQuantity.innerHTML = sumQuantity;
-
-      for (const itemQuantityValues of itemQuantityValue) {
-        itemQuantityValues.addEventListener("change", function (event) {
-          sumQuantity += Number(event.target.value);
-          totalQuantity.innerHTML = sumQuantity;
-
-          let total =
-            Number(event.target.value) * Number(cartItemsArray[i].price);
-          sumPrice += total;
-          totalPrice.innerHTML = sumPrice;
-        });
-      }
-    }
+function createCart() {
+  const cart = [];
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    const sofa = localStorage.getItem(key);
+    cart.push(JSON.parse(sofa));
   }
+  return cart;
 }
 
-function deleteSofa() {
-  deleteItems.addEventListener(
-    "click",
-    function () {
-      alert("delete click");
-    },
-    { once: true }
-  );
+function showItemsinCart() {
+  const cartItems = document.getElementById("cart__items");
+  if (cartItems == null) return;
+  for (const sofa of cart) {
+    const item = createItem(sofa);
+    cartItems.appendChild(item);
+
+    totalListener(item, sofa);
+    deleteButton(item);
+  }
+  total(cart);
+}
+
+function createItem(sofa) {
+  const item = document.createElement("item");
+  item.classList.add("cart__item");
+  item.setAttribute("data-id", sofa._id);
+  item.setAttribute("data-color", sofa.selectedColor);
+  item.innerHTML = `
+                    <div class="cart__item__img">
+                    <img src=${sofa.imageUrl} alt=${sofa.altTxt}>
+                    </div>
+                    <div class="cart__item__content">
+                    <div class="cart__item__content__description">
+                        <h2>${sofa.name}</h2>
+                        <p>${sofa.selectedColor}</p>
+                        <p>${sofa.price}</p>
+                    </div>
+                    <div class="cart__item__content__settings">
+                        <div class="cart__item__content__settings__quantity">
+                        <p>Qté : </p>
+                        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${sofa.selectedQuantity}>
+                        </div>
+                        <div class="cart__item__content__settings__delete">
+                        <p class="deleteItem">Delete</p>
+                        </div>
+                    </div>
+                    </div>
+                </article>
+    `;
+  return item;
+}
+
+function total(sofa) {
+  let numberOfSofas = 0;
+  let totalPriceSofas = 0;
+
+  sofa.forEach((sofa) => {
+    numberOfSofas += sofa.selectedQuantity;
+    totalPriceSofas += sofa.selectedQuantity * sofa.price;
+  });
+
+  updateTotal(numberOfSofas, totalPriceSofas);
+}
+
+function updateTotal(numberOfSofas, totalPriceSofas) {
+  const totalQuantity = document.getElementById("totalQuantity");
+  const totalPrice = document.getElementById("totalPrice");
+
+  totalQuantity.innerHTML = numberOfSofas;
+  totalPrice.innerHTML = totalPriceSofas;
+}
+
+function totalListener(item, sofa) {
+  item.querySelector(".itemQuantity").addEventListener("change", (event) => {
+    sofa.selectedQuantity = Number(event.target.value);
+    cart.selectedQuantity = sofa.selectedQuantity;
+    total(cart);
+  });
+}
+
+function deleteButton(item) {
+  const deleteButton = item.querySelector(".deleteItem");
+  deleteButton.addEventListener("click", () => {
+    const key = article.dataset.id + ":" + article.dataset.color;
+    window.localStorage.removeItem(key);
+    window.location.reload();
+  });
 }
 
 const contact = {
@@ -102,26 +100,72 @@ const contact = {
   email: document.getElementById("email"),
 };
 
-document.getElementById("order").addEventListener("submit", () => {
-  alert(contact);
-  sendOrder();
-});
+function sendForm() {
+  const firstName = document.getElementById("firstName");
+  const firstNameErrorMsg = document.getElementById("firstNameErrorMsg");
+  const noNumbers = new RegExp(/^[a-z]*$/);
+  firstName.addEventListener("change", () => {
+    checkInput(firstName, noNumbers, firstNameErrorMsg, "first name");
+  });
+
+  const lastName = document.getElementById("lastName");
+  const lastNameErrorMsg = document.getElementById("lastNameErrorMsg");
+  lastName.addEventListener("change", () => {
+    checkInput(lastName, noNumbers, lastNameErrorMsg, "name");
+  });
+  const address = document.getElementById("address");
+
+  address.addEventListener("change", () => {});
+  const city = document.getElementById("city");
+  const cityErrorMsg = document.getElementById("cityErrorMsg");
+  city.addEventListener("change", () => {
+    checkInput(city, noNumbers, cityErrorMsg, "city");
+  });
+  const email = document.getElementById("email");
+  const emailErrorMsg = document.getElementById("emailErrorMsg");
+  const emailReg = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+  email.addEventListener("change", () => {
+    checkInput(email, emailReg, emailErrorMsg, "email");
+  });
+}
+
+function checkInput(name, reg, err, text) {
+  if (reg.test(name.value) == false) {
+    console.log(reg.test(name.value));
+    err.innerHTML = "Please add correct " + text;
+  } else {
+    err.innerHTML = " ";
+  }
+}
+
+function order() {
+  document.getElementById("order").addEventListener("click", (event) => {
+    sendOrder();
+  });
+}
 
 function sendOrder() {
-  let form = {
-    firstName: firstNameInput.value,
-    secondName: secondNameInput.value,
-    address: addressInput.value,
-    city: cityInput.value,
-    email: emailInput.value,
+  let body = {
+    contact: {
+      firstName: firstName.value,
+      lastName: lastName.value,
+      address: address.value,
+      city: city.value,
+      email: email.value,
+    },
   };
-
-  fetch("http://localhost:3000/api/products/order", {
+  const url = `http://127.0.0.1:3000/api/products/order`;
+  fetch(url, {
     method: "POST",
     headers: {
-      Accept: "application/json",
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(form),
-  });
+    body: JSON.stringify(body),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const orderId = Math.floor(Math.random() * 100);
+      window.location.href = "../html/confirmation.html?orderId=" + orderId;
+    })
+    .catch((err) => alert(err));
 }
--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,15 +1,5 @@
-const orderNumber = window.location.search.split("=").at(-1);
-const url = `http://127.0.0.1:5500/html/cart`;
-fetch(url)
-  .then((response) => {
-    const json = response.json();
-    return json;
-  })
-  .then((data) => {
-    displayItem(data);
-    orderNumber = data;
-  })
-  .catch((err) => console.log(err));
+const params = new URL(window.location.href).searchParams.get("orderId");
+console.log(params);
 
-const orderId = data;
-document.getElementById("orderId").textContent = `${orderNumber._id}`;
+document.getElementById("orderId").innerHTML = params;
+localStorage.clear();
